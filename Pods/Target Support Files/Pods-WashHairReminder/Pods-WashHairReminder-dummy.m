3c167410d3853f84d6fd393ca8c140d832353b58
Zhuo Li <zhuozhuozhuol@Zhuos-iMac.local>
2017-03-23 22:02:39 -0700

    Add date picker option.

---
+++ Pods/Target Support Files/Pods-WashHairReminder/Pods-WashHairReminder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WashHairReminder : NSObject
+@end
+@implementation PodsDummy_Pods_WashHairReminder
+@end
--- WashHairReminder/Analysis.swift
+++ WashHairReminder/Analysis.swift
@@ -6,9 +6,14 @@
 //  Copyright © 2016年 Allen. All rights reserved.
 //
 
+import DateTimePicker
 import Foundation
 
 class Analysis : UIViewController {
+    
+    @IBOutlet weak var pick: UIButton!
+    
+    var current = Date()
     
     override func viewDidLoad() {
         UIApplication.shared.isStatusBarHidden = true
@@ -17,5 +22,22 @@ class Analysis : UIViewController {
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         
     }
+    
+    @IBAction func showDateTimePicker(_ sender: AnyObject) {
+        let min = Date().addingTimeInterval(-60 * 60 * 24 * 4)
+        let max = Date().addingTimeInterval(60 * 60 * 24 * 4)
+        let picker = DateTimePicker.show(selected: current, minimumDate: min, maximumDate: max)
+        picker.highlightColor = UIColor(red: 255.0/255.0, green: 138.0/255.0, blue: 138.0/255.0, alpha: 1)
+        picker.doneButtonTitle = "!! DONE DONE !!"
+        picker.todayButtonTitle = "Today"
+        picker.completionHandler = { date in
+            self.current = date
+            let formatter = DateFormatter()
+            formatter.dateFormat = "HH:mm dd/MM/YYYY"
+            self.pick.setTitle(formatter.string(from: date), for: .normal)
+            print(formatter.string(from: date))
+        }
+    }
+
     
 }
